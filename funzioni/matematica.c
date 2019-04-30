d6f86600daca8054eb7a8de1cafcd914bf3c1895
Lorenzo <brixiumx@gmail.com>
2019-04-30 20:58:54 +0200

    Update matemetica

--- funzioni/matematica.c
+++ funzioni/matematica.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-int sommaSemplice(int);
+int sommatoriaSemplice(int);
 int coefficienteBinomiale(int, int);
+int fibonacci(int);
+int potenza(int, int);
 
 int main(int argc, char * argv[]){
-	printf("%d\n", sommaSemplice(37));
+	printf("%d\n", sommatoriaSemplice(37));
 	printf("%d\n", coefficienteBinomiale(8, 4));
+	printf("%d\n", fibonacci(139));
+	printf("%d\n", potenza(2, 25));
 	return 0;
 }
 
-int sommaSemplice(int fine){
+int sommatoriaSemplice(int fine){
 	return (fine+1)*fine/2;
 }
 
@@ -18,3 +22,23 @@ int coefficienteBinomiale(int n, int k){
 		return 1;
 	return coefficienteBinomiale(n, k+1) * (k+1) / (n-k);
 }
+
+int fibonacci(int fin){
+	int prev, cur, aux;
+	cur = 1;
+	prev = 0;
+	while(cur < fin){
+		printf("%d ",cur);
+		aux = cur;
+		cur = prev + cur;
+		prev = aux;
+	}
+	return cur;
+}
+
+int potenza(int base, int exp){
+	if(exp == 0)
+		return 1;
+	return potenza(base, exp-1) * base;
+}
+
