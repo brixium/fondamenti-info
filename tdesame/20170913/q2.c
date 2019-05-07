8153b835cf357d88cf1e5dd8e8b9106d73287958
Lorenzo <brixiumx@gmail.com>
2019-05-07 15:36:55 +0200

    q2.c (varianza) done!

--- tdesame/20170913/q2.c
+++ tdesame/20170913/q2.c
@@ -2,116 +2,56 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
-#define DIM 5
-/*SI scriva un sottoprogrmma che ricevuto in ingresso un array x di valori interi ed un valore intero n e qualsiasi parametro ritenuto strettamente necessario trasmette al chiamante l'array di valori che costituiscono la varianza x calcolata su n campioni. L'elemento i-esimo della varianza viene calcolato come varianza degli n valori del vettore in ingresso che precedono ed includono l'elemento i. Se l'elemento i è preceduto da meno di n-1 valori, la varianza si calcola su quelli.*/
 
-float * varianza(int [], int, int);
-float * varianza3(int [], int, int);
+#define DIM 5
+
+/*Si scriva un sottoprogramma che ricevuto in ingresso un array x di valori interi ed un valore intero n e qualsiasi parametro ritenuto strettamente necessario trasmette al chiamante l'array di valori che costituiscono la varianza x calcolata su n campioni. L'elemento i-esimo della varianza viene calcolato come varianza degli n valori del vettore in ingresso che precedono ed includono l'elemento i. Se l'elemento i è preceduto da meno di n-1 valori, la varianza si calcola su quelli.*/
+
+/*varianza riceve: array di valori interi, dimensione finestra n (intero), dimensione array (int), riferimento in memoria a varianza (1 array di float)*/
+void varianza(int[], int, int, float ** v);
 
 int main(int argc, char * argv[]){
-	int val[DIM], gap, i;
-	float * risultato;
-	gap = 3;
+	int val[DIM], n, dimx;
+	float * verza;
 	
-	/*init*/
-	val[0] = 10;
-	val[1] = 2;
-	val[2] = 3;
-	val[3] = 13;
-	val[4] = 101;
-	/*richiamo varianza*/
-	risultato = varianza3(val, gap, DIM);
-	/*stampa del ris*/
+	int i;
+	verza = malloc(sizeof(float)*DIM);
+	val[0] = 7;
+	val[1] = 5;
+	val[2] = 8;
+	val[3] = 4;
+	val[4] = 6;
+	n = 3;
+	dimx = DIM;
+	
+	varianza(val, n, dimx, &verza);
+
 	for(i=0; i<DIM; i++)
-		printf("%f ", *(risultato+i));
+		printf("%f; ", *(verza+i));
 	printf("\n");
 	return 0;
 }
 
-float * varianza3(int x[], int n, int dimx){
-	/*Dichiarazione variabili*/
-	float * varianza = NULL;
-	float media, parz;
-	int i, j, k;
-
-	/*Ciclare per tutti i valori dell'array*/
-	for(j=0; j<dimx; j++){
-		/*Ciclare per una finestra di n numeri max*/
+void varianza(int x[], int n, int dimx, float ** varia){
+	int i, j, startpt;
+	float media, somma;
+	
+	for(i=0; i<dimx; i++){
+		/*Qui decido il punto di partenza del ciclo per la somma delle medie*/
+		if(i+1-n > 0)
+			startpt = i+1-n;
+		else
+			startpt = 0;
+		/*Calcolo delle medie*/
 		media = 0;
-		parz = 0;
-		for(i=0; i<n; i++){
-			/*Calcolo della media*/
-			if(j<n){
-				for(k=0; k<=j; k++)
-					media = media + x[i];
-				media = media / k;
-				printf("Media: %f\n", media);
-				/*Calcolo varianza per elemento j-esimo*/
-				if((varianza = (float *)malloc(sizeof(float)))){
-					for(k=0; k<j; k++){
-						parz = parz + ((x[k] - media)*(x[k] - media));
-					}
-					parz = parz / k;
-					*(varianza+k) = parz;
-				}else
-					printf("Errore allocazione memoria\n");
-			}else{
-				for(k=j-n; k<n; k++)
-					media = media + x[i];
-				media = media / n;
-			}
-			
-		}
+		for(j=startpt; j<=i; j++)
+			media = media+x[j];
+		media = media / (j-startpt);
+		/*qua si calcola la varianza*/
+		somma = 0;
+		for(j=startpt; j<=i; j++)
+			somma = somma + pow(x[j]-media, 2);
+		somma = somma / (j-startpt);
+		*(*varia+i) = somma;
 	}
-	return varianza;
 }
-/*
-float * varianza2(int x[], int n, int dimx){
-	float * ret = NULL;
-	int i, j, mpar;
-
-	mpar = 0;
-	for(j=0; j<dimx; j++){
-		for(i=0; i<=n && i<=j; i++){
-			if(n<j)
-				div = n;
-			else
-				div = j;
-			for(k=i; k<div; k++)
-				mpar = mpar + x[k];
-			ret[j] = ret[j] + (x[i] - (mpar/div) * (x[i] - (mpar/div) );
-			
-		}
-		ret[j] = ret[j] / n;
-	}
-	return ret;
-}/
-
-float * varianza(int x[], int n, int dimx){
-	int i, j, dim;
-	float *ris, media, part;
-
-	dim = dimx;
-	media = x[0];
-	part = 0;
-	if((ris = (float *)malloc(sizeof(float)*dimx))){
-
-		for(i=0; i<dim; i++){
-			for(j=0; j<=i && j<n;j++){
-				part = part + pow(x[j] - media, 2);
-				printf("part: %f, i: %d, j: %d\n", part, i, j);
-				if(i<3)
-					media = part / (i+1);
-				else
-					media = part / n;
-			}
-			*(ris+i) = sqrt(part / (dim-1));
-			part = 0.0;
-			media = 0.0;
-		}
-		return ris;
-	}else
-		printf("Errore nell'allocazione di spazio heap");
-	return NULL;
-}
-*/
