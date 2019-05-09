95f0e86c41ec05d9b5c19542e18cd2e09729e148
Lorenzo <brixiumx@gmail.com>
2019-05-09 20:00:05 +0200

    q4 done

--- tdesame/20180702/q4.c
+++ tdesame/20180702/q4.c
@@ -12,31 +12,35 @@ int main(int argc, char * argv[]){
 	int len, i, j, flag, * diff1, * diff2;
 	char * str, * reverse;
 
+	if(!argv[1]){
+		printf("Aggiungi una stringa come argomento\n");
+		return 1;
+	}
+
 	len = strlen(argv[1]);
-	if((str = (char *)malloc(sizeof(char)*len))){
-		strcpy(str, argv[1]);
-		if((reverse = (char *)malloc(sizeof(char)*len))){
-			for(i=0, j=len-1; i<len; i++, j--)
-				*(reverse+i) = *(str+j);
-			if((diff1 = (int *)malloc(sizeof(int)*len)) && (diff2 = (int *)malloc(sizeof(int)*len))){
-				flag = 1;
-				for(i=0; i<len-1 && flag; i++){
-					*(diff1+i) = calcolaDiff(*(str+i), *(str+i+1));
-					*(diff2+i) = calcolaDiff(*(reverse+i), *(reverse+i+1));
-					if(*(diff1+i) != *(diff2+i))
-						flag = 0;
-				}
-				printf("%d\n", flag);
-				free(diff1);
-				free(diff2);
-			}else
-				printf("Errore allocazione memoria arr int");
-			free(str);
+	str = argv[1];
+	strcpy(str, argv[1]);
+	/*printf("%c\n", str[0]);*/
+	if((reverse = (char *)malloc(sizeof(char)*len))){
+		for(i=0, j=len-1; i<len; i++, j--)
+			*(reverse+i) = *(str+j);
+		if((diff1 = (int *)malloc(sizeof(int)*len)) && (diff2 = (int *)malloc(sizeof(int)*len))){
+			flag = 1;
+			for(i=0; i<len-1 && flag; i++){
+				*(diff1+i) = calcolaDiff(*(str+i), *(str+i+1));
+				*(diff2+i) = calcolaDiff(*(reverse+i), *(reverse+i+1));
+				if(*(diff1+i) != *(diff2+i))
+					flag = 0;
+			}
+			printf("%d\n", flag);
+			free(diff1);
+			free(diff2);
 		}else
-			printf("Err. alloc. mem. 2nda str.");
+			printf("Errore allocazione memoria arr int");
+
 		free(reverse);
 	}else
-		printf("Errore nell'allocazione di memoria prima stringa");
+		printf("Err. alloc. mem. 2nda str.");
 	return 0;
 }
 
