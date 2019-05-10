64beaf8b70fed904c04fc59a12c91528d849341c
L.A <brixiumx@gmail.com>
2019-05-10 19:58:17 +0200

    removed comment

--- tdesame/20180702/q5.c
+++ tdesame/20180702/q5.c
@@ -1,5 +1,3 @@
-/*DISCLAIMER: QUESTO PROGRAMMA NON È STATO NÉ COMPILATO NÉ TESTATO E PERTANTO POTREBBE NON FUNZIONARE, ANCHE PERCHÉ MANCA IL CODICE DELLA FUNZIONE INSERTUP CHE INSERISCE L'ELEMENTO IN ORDINE CRESCENTE IN UNA LISTA*/
-
 /*
 Scrivere un sottoprogramma in C selectitems che ricevuta in ingresso una lista per la gestione di valori interi e due interi min e max crea una nuova lista contente tutti e soli i valori presenti nella lista di ingresso strettamente compresi tra min e max, ordinati in modo crescente e la restituisce al chiamante. La lista di partenza non deve essere modificata. Si considerino dati i sottoprogrammi i cui prototipi sono riportati nel seguito.
 */
